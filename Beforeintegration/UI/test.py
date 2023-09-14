6f5aab99d1f022c0450495219d1742351924b610
Inoue Kanata <c1p31014@bunkyo.ac.jp>
2023-09-14 14:15:33 +0900

    次の機能を追加 ・画像のリサイズ ・表の2行目の追加 表の二行目は画像がかぶっているため、要修正

--- Beforeintegration/UI/test.py
+++ Beforeintegration/UI/test.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from PIL import Image
+
 root = tk.Tk()
 root.geometry('500x500')
 tree = ttk.Treeview(root, column=('A','B'), selectmode='browse', height=10)
@@ -9,6 +11,11 @@ tree.heading('#1', text='A', anchor='center')
 tree.heading('#2', text='B', anchor='center')
 tree.column('A', anchor='center', width=100)
 tree.column('B', anchor='center', width=100)
-img = tk.PhotoImage(file="9K.png")
+im = Image.open(r"C:\ProgramFolder\branch\proed\Beforeintegration\UI\9k.png")
+back_im = im.copy()
+back_im = back_im.resize((180, 110))
+back_im.save('9k_resize.png', quality=95)
+img = tk.PhotoImage(file='9k_resize.png')
 tree.insert('', 'end', text="#0's text", image=img, value=("Something", "Another Thing"))
+tree.insert('', 'end', text="#0's text", image=img, value=("2Something", "2Another Thing"))
 root.mainloop()
